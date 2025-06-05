babd6046fcc0917a7dc32735e10e9c22430d54ce
Arturo J. Pérez <arturjosep@gmail.com>
2025-06-05 23:57:30 +0200

    Handle non standard resolutions with SAR and DAR

--- reference_importer_main/imageSequence.py
+++ reference_importer_main/imageSequence.py
@@ -6,7 +6,8 @@ import platform
 from pathlib import Path
 
 TIMECODE_REGEX = r"Duration: (\d{2}:\d{2}:\d{2}.\d)"
-
+DIMENSIONS_REGEX = r"Video.*[ ,](\d+)x(\d+),"
+DIMENSIONS_SAR_DAR_REGEX = r"(\d+)x(\d+) \[SAR (\d+):(\d+) DAR (\d+):(\d+)\]"
 
 class FfmpegError(Exception):
     ...
@@ -14,6 +15,13 @@ class FfmpegError(Exception):
 class MissingFfmpeg(FfmpegError):
     ...
 
+class Dimensions:
+    w: int
+    h: int
+    sar_w: int
+    sar_h: int
+    dar_w: int
+    dar_h: int
 
 class ImageSequencer:
     def __init__(
@@ -91,6 +99,57 @@ class ImageSequencer:
         duration: str = str(match.groups(0)[0])
         return duration
 
+    def get_dimensions(self, video_file: str) -> Dimensions:
+        """Gets the dimensions of the video from the output of ffmpeg -i.
+
+        Args:
+            video_file: Video file.
+
+        Raises:
+            FfmpegError: If ffmpeg errors out and does not yield an output.
+            ValueError: If there is no match for the dimensions in ffmpeg's output.
+
+        Returns:
+            Dimensions of the video.
+        """
+        command = f'"{self.ffmpeg_path}" -i "{video_file}"'
+        try:
+            output = str(
+                subprocess.run(
+                    command,
+                    shell=True,
+                    stderr=subprocess.STDOUT,
+                    stdout=subprocess.PIPE,
+                    check=False,
+                ),
+            )
+
+        except subprocess.CalledProcessError as e:
+            _except_msg: str = "Unable to get ffmpeg output"
+            raise FfmpegError(_except_msg) from e
+
+        with_sar_dar: bool = True
+        # re.search searches across the whole string (multiline output).
+        match: re.Match | None = re.search(DIMENSIONS_SAR_DAR_REGEX, output)
+
+        if not match:
+            with_sar_dar = False
+            match: re.Match | None = re.search(DIMENSIONS_REGEX, output)
+
+        if not match:
+            _except_msg: str = f"Unable to find Dimensions for video {video_file}"
+            raise ValueError(_except_msg)
+
+        dimensions = Dimensions()
+        dimensions.w = int(match.groups(0)[0])
+        dimensions.h = int(match.groups(0)[1])
+        dimensions.sar_w = int(match.groups(0)[2]) if with_sar_dar else 1
+        dimensions.sar_h = int(match.groups(0)[3]) if with_sar_dar else 1
+        dimensions.dar_w = int(match.groups(0)[4]) if with_sar_dar else 1
+        dimensions.dar_h = int(match.groups(0)[5]) if with_sar_dar else 1
+
+        return dimensions
+
     def create_sequence(
         self,
         input_file: str,
@@ -99,9 +158,12 @@ class ImageSequencer:
         end_trim: str,
         output_file: str,
     ):
+        dimensions = self.get_dimensions(input_file)
+        scale_x = dimensions.w
+        scale_y = round(dimensions.h * (float(dimensions.sar_h) / float(dimensions.sar_w)))
         command = (
             f'"{self.ffmpeg_path}" -i "{input_file}" -r {frame_rate}'
-            f" -vf scale=1280:-1 -q:v 3 -ss {start_trim} -to {end_trim} "
+            f" -vf scale={scale_x}:{scale_y} -q:v 3 -ss {start_trim} -to {end_trim} "
             f'"{output_file}"'
         )
         try:
